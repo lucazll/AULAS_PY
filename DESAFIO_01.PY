4fdf584b37f401e37e45d4fce7c55b7391e1ebdb
ead <ead@academico.local>
2024-08-22 08:52:26 -0300

    first commit

---
+++ DESAFIO_01.PY
@@ -0,0 +1,6 @@
+celsius = int(input('Digite uma temperatura em graus celsius: '))
+
+fahrenheit = celsius * 1.8 + 32
+kelvin = celsius + 273
+
+print(f'O valor de Fahrenheit é {fahrenheit:.2f} e o valor de Kelvin é {kelvin:.2f}.')
---
+++ DESAFIO_03.PY
@@ -0,0 +1,6 @@
+metros = int(input('Digite um valor de medida em metros:'))
+
+cent = metros * 100
+mili = metros * 1000
+
+print(f'O valor inserido em centímetros é de {cent} e o valor em milímetros é de {mili}.')
